6f6a9aac13977eadf413e6cc8dc360a3247d92a9
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-11 08:37:26 -0300

    Simulacro completo

---
+++ Assets/Scripts/Simulacro.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulacro : MonoBehaviour
+{
+
+    public string gustoHelado;
+    public float gramos;
+    float precioGramo = 1.25f;
+    float precioFinal;
+    void Start()
+    {
+        
+
+        if (gramos < 250 || gramos > 3000)
+        {
+            Debug.Log("ERROR: Cantidad de gramos menor a 250 o mayor a 3000");
+        } else
+        {
+            if (gustoHelado == "CHO" || gustoHelado == "DDL")
+            {
+                precioFinal = precioGramo * gramos;
+                Debug.Log("Costo total: " + precioFinal + "$");
+            } else if (gustoHelado == "FRU")
+            {
+                precioFinal = precioGramo * gramos * 0.90f;
+                Debug.Log("Costo total: " + precioFinal + "$");
+            } else
+            {
+                Debug.Log("ERROR: Codigo no valido");
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
